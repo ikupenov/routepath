6744b7311b59229863230c91831f2e1eb3facac1
Iliyan Kupenov <i.kupenov96@gmail.com>
2023-01-12 00:27:35 +0200

    feat(cli): add init cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,38 @@
-export * from "./generator"
+import yargs from "yargs"
+
+yargs.usage("\nUsage: $0 [cmd] <args>").alias("h", "help")
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note")
+  },
+})
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a Note",
+  handler: () => {
+    console.log("removing note")
+  },
+})
+
+yargs.parse()
+
+// yargs
+//   .usage("HELLO FROM CLI")
+//   .option("l", {
+//     alias: "language",
+//     describe: "Translate to language",
+//     type: "string",
+//     demandOption: false,
+//   })
+//   .option("s", {
+//     alias: "sentence",
+//     describe: "Sentence to be translated",
+//     type: "string",
+//     demandOption: false,
+//   })
+//   .help(true)
+//   .parse()
